c9cd9bec9a06c9fd76a1e5762b3115ccda2f097d
Angus Meng <mokton@users.noreply.github.com>
2017-08-20 10:13:56 +0800

    Update wifi.py
    Indent changes. If wifi has already connected, not connect again.

--- wifi.py
+++ wifi.py
@@ -1,17 +1,18 @@
 def do_connect():
-        import network
-        sta_if = network.WLAN(network.STA_IF)
-        ap_if = network.WLAN(network.AP_IF)
-        if ap_if.active():
-                ap_if.active(False)
-        if not sta_if.isconnected():
-                print('connecting to network...')
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    if ap_if.active():
+        ap_if.active(False)
+    if not sta_if.isconnected():
+        print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('SSID', 'PASSOWRD') 
+        sta_if.connect('SSID', 'PASSOWRD')
         import time
         while not sta_if.isconnected():
-                time.sleep(1)
-                print('.',)
-        print('network config:', sta_if.ifconfig())
+            time.sleep(1)
+            print('.',)
+    print('')
+    print('network config:', sta_if.ifconfig())
 
 #do_connect()
